b35ea044f4b28301da5e035d577a887e9a3e3e4e
Sujith S <sujith@sj.local>
2025-03-01 17:39:05 +0530

    Windows compatible frontend: mic access issues fixes; not bug free

--- frontend/src-tauri/build.rs
+++ frontend/src-tauri/build.rs
@@ -1,15 +1,5 @@
 fn main() {
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-lib=framework=AVFoundation");
-    
-    #[cfg(target_os = "windows")]
-    {
-        // Add Windows-specific build configurations
-        println!("cargo:rustc-link-lib=ole32");
-        println!("cargo:rustc-link-lib=user32");
-        println!("cargo:rustc-link-lib=avrt");
-        println!("cargo:rustc-link-lib=ksuser");
-    }
-    
     tauri_build::build()
 }
